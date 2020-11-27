932a87d17c32fe81b98a69070e1edeadfa1c41d7
Jonny Larsson <jonny@gardnet.nu>
2020-11-27 15:54:59 +0100

    Basic Blazor Template but with MudBlazor

---
+++ MudBlazor.Template.Blazor/MudBlazor.Blazor.Wasm/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MudBlazor;
+using MudBlazor.Dialog;
+using MudBlazor.Services;
+
+namespace MudBlazor.Blazor.Wasm
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudBlazorDialog();
+            builder.Services.AddMudBlazorSnackbar();
+            builder.Services.AddMudBlazorResizeListener();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
