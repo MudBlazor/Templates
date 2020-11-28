ab7f69959d1627a54a9e7c8d50050764ba87df2f
Christian Weyer <christian.weyer@thinktecture.com>
2020-11-28 19:19:36 +0100

    💄and cleanup

--- MudBlazor.Template.Blazor/MudBlazor.Blazor.Wasm/Program.cs
+++ MudBlazor.Template.Blazor/MudBlazor.Blazor.Wasm/Program.cs
@@ -1,14 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using MudBlazor;
-using MudBlazor.Dialog;
 using MudBlazor.Services;
 
 namespace MudBlazor.Blazor.Wasm
@@ -21,7 +15,7 @@ namespace MudBlazor.Blazor.Wasm
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             builder.Services.AddMudBlazorDialog();
             builder.Services.AddMudBlazorSnackbar();
             builder.Services.AddMudBlazorResizeListener();
