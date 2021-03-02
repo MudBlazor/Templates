4694ecaeb5c5af87843693f0e5be39cd06019adf
Jonny Larsson <jonny@gardnet.nu>
2021-03-02 01:39:46 +0100

    Updated DefaultBlazor Templates

---
+++ MudBlazor.Template.DefaultBlazor/DefaultBlazor.Wasm/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MudBlazor.Services;
+
+namespace DefaultBlazor.Wasm
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMudServices();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
