457f00265cbacd35ec292bdfa6b43efa9f8e581e
grbd <hecatrons.workshop@gmail.com>
2021-06-03 16:50:17 +0100

    added initial BlazorWebView WinForms example

---
+++ MudBlazor.Template.DefaultBlazor/DefaultBlazor.WinForms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using DefaultBlazor.WinForms.Data;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace DefaultBlazor.WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddBlazorWebView();
+            serviceCollection.AddMudServices();
+            serviceCollection.AddSingleton<HttpClient>();
+            serviceCollection.AddSingleton<WeatherForecastService>();
+            InitializeComponent();
+
+            var blazor = new BlazorWebView()
+            {
+                Dock = DockStyle.Fill,
+                HostPage = "wwwroot/index.html",
+                Services = serviceCollection.BuildServiceProvider(),
+            };
+            blazor.RootComponents.Add<App>("#app");
+            Controls.Add(blazor);
+        }
+
+    }
+}
