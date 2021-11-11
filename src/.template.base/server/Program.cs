9cc3521e839e4d87ca207121bb41f15dcf63b79b
Sijmen Koffeman <koffemans@hotmail.com>
2021-11-11 21:41:49 +0000

    UseStaticWebAssets() on webBuilder in Program.cs to prevent web assets not being loaded in environments other than Development

--- src/.template.base/server/Program.cs
+++ src/.template.base/server/Program.cs
@@ -20,6 +20,7 @@ namespace MudBlazor.Template
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseStaticWebAssets();
                     webBuilder.UseStartup<Startup>();
                 });
     }
