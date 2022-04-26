d9eae62518c08199c4cd0c6a4c53d02bbe217d60
Sijmen Koffeman <koffemans@hotmail.com>
2022-04-26 16:50:42 +0100

    Added StaticWebAssests loader to prevent 404 when using a different ASPNETCORE_ENVIRONMENT

--- src/.template.base/wasm-hosted/Server/Program.cs
+++ src/.template.base/wasm-hosted/Server/Program.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.ResponseCompression;
+﻿using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
 // Add services to the container.
 
