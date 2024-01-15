30d8e2ba36d723b35bc6710585f3818c8538ac09
Jonny Larsson <jonny@gardnet.nu>
2024-01-15 02:06:52 +0100

    Feature/new templates (#448)
    * Cleanup
    
    * Rename
    
    * Reorganize
    
    * Pack Project
    
    * Update README.md

---
+++ src/mudblazor/MudBlazor.Template/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using MudBlazor.Template.Data;
+
+namespace MudBlazor.Template.Components.Account;
+
+internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+{
+    public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+    {
+        var user = await userManager.GetUserAsync(context.User);
+
+        if (user is null)
+        {
+            redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+        }
+
+        return user;
+    }
+}
