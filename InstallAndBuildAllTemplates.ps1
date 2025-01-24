5d0db48649f6b086f71c23c456199827a238b250
ScarletKuro <artem.melnikov@live.com>
2025-01-24 19:07:48 +0200

    Update script

--- InstallAndBuildAllTemplates.ps1
+++ InstallAndBuildAllTemplates.ps1
@@ -14,17 +14,17 @@ dotnet build $(Join-Path $tests '/InteractivityAuto') /warnaserror
 dotnet new mudblazor --interactivity Server --output $(Join-Path $tests 'InteractivityServer')
 dotnet build $(Join-Path $tests 'InteractivityServer') /warnaserror
 
-dotnet new mudblazor --interactivity WebAssembly --output $(Join-Path $tests 'InteractivityWebAssembly')
-dotnet build $(Join-Path $tests 'InteractivityWebAssembly') /warnaserror
+dotnet new mudblazor --interactivity WebAssembly --output $(Join-Path $tests 'InteractivityWasm')
+dotnet build $(Join-Path $tests 'InteractivityWasm') /warnaserror
 
-dotnet new mudblazor --interactivity Auto --output $(Join-Path $tests 'InteractivityAuto_AllInteractive') --all-interactive
-dotnet build $(Join-Path $tests 'InteractivityAuto_AllInteractive') /warnaserror
+dotnet new mudblazor --interactivity Auto --output $(Join-Path $tests 'InteractivityAuto_Global') --all-interactive
+dotnet build $(Join-Path $tests 'InteractivityAuto_Global') /warnaserror
 
-dotnet new mudblazor --interactivity Server --output $(Join-Path $tests 'InteractivityServer_AllInteractive') --all-interactive
-dotnet build $(Join-Path $tests 'InteractivityServer_AllInteractive') /warnaserror
+dotnet new mudblazor --interactivity Server --output $(Join-Path $tests 'InteractivityServer_Global') --all-interactive
+dotnet build $(Join-Path $tests 'InteractivityServer_Global') /warnaserror
 
-dotnet new mudblazor --interactivity WebAssembly --output $(Join-Path $tests 'InteractivityWebAssembly_AllInteractive') --all-interactive
-dotnet build $(Join-Path $tests 'InteractivityWebAssembly_AllInteractive') /warnaserror
+dotnet new mudblazor --interactivity WebAssembly --output $(Join-Path $tests 'InteractivityWasm_Global') --all-interactive
+dotnet build $(Join-Path $tests 'InteractivityWasm_Global') /warnaserror
 
 dotnet new mudblazor --interactivity Auto --output $(Join-Path $tests 'InteractivityAuto_Auth') --auth Individual
 dotnet build $(Join-Path $tests 'InteractivityAuto_Auth') /warnaserror
@@ -32,11 +32,11 @@ dotnet build $(Join-Path $tests 'InteractivityAuto_Auth') /warnaserror
 dotnet new mudblazor --interactivity Server --output $(Join-Path $tests 'InteractivityServer_Auth') --auth Individual
 dotnet build $(Join-Path $tests 'InteractivityServer_Auth') /warnaserror
 
-dotnet new mudblazor --interactivity WebAssembly --output $(Join-Path $tests 'InteractivityWebAssembly_Auth') --auth Individual
-dotnet build $(Join-Path $tests 'InteractivityWebAssembly_Auth') /warnaserror
+dotnet new mudblazor --interactivity WebAssembly --output $(Join-Path $tests 'InteractivityWasm_Auth') --auth Individual
+dotnet build $(Join-Path $tests 'InteractivityWasm_Auth') /warnaserror
 
-dotnet new mudblazor --interactivity Auto --output $(Join-Path $tests 'InteractivityAuto_AllInteractive_Auth') --all-interactive --auth Individual
-dotnet build $(Join-Path $tests 'InteractivityAuto_AllInteractive_Auth') /warnaserror
+dotnet new mudblazor --interactivity Auto --output $(Join-Path $tests 'InteractivityAuto_Global_Auth') --all-interactive --auth Individual
+dotnet build $(Join-Path $tests 'InteractivityAuto_Global_Auth') /warnaserror
 
 dotnet new mudblazor --interactivity Auto --output $(Join-Path $tests 'InteractivityAuto_UseMain') --use-program-main
 dotnet build $(Join-Path $tests 'InteractivityAuto_UseMain') /warnaserror
