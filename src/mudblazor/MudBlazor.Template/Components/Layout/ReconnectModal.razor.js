538a718c62ffc3b34c217b56966f1612c11772ce
ScarletKuro <artem.melnikov@live.com>
2026-02-22 01:04:03 +0200

    Fix ReconnectModal.razor.js

--- src/mudblazor/MudBlazor.Template/Components/Layout/ReconnectModal.razor.js
+++ src/mudblazor/MudBlazor.Template/Components/Layout/ReconnectModal.razor.js
@@ -1,51 +1,63 @@
-const modal = document.getElementById('components-reconnect-modal');
-const reconnectButton = document.getElementById('components-reconnect-button');
-const resumeButton = document.getElementById('components-resume-button');
-const secondsToNextAttempt = document.getElementById('components-seconds-to-next-attempt');
+// Set up event handlers
+const reconnectModal = document.getElementById("components-reconnect-modal");
+reconnectModal.addEventListener("components-reconnect-state-changed", handleReconnectStateChanged);
 
-reconnectButton?.addEventListener('click', () => Blazor.reconnect());
-resumeButton?.addEventListener('click', () => Blazor.resume());
+const retryButton = document.getElementById("components-reconnect-button");
+retryButton.addEventListener("click", retry);
 
-Blazor.registerCustomEventType?.('reconnect', {});
+const resumeButton = document.getElementById("components-resume-button");
+resumeButton.addEventListener("click", resume);
 
-window.Blazor.start({
-    circuit: {
-        reconnectionHandler: {
-            onConnectionDown(options, _error) {
-                modal?.removeAttribute('class');
-                if (!modal?.hasAttribute('open')) {
-                    modal?.showModal();
-                }
+function handleReconnectStateChanged(event) {
+    if (event.detail.state === "show") {
+        reconnectModal.showModal();
+    } else if (event.detail.state === "hide") {
+        reconnectModal.close();
+    } else if (event.detail.state === "failed") {
+        document.addEventListener("visibilitychange", retryWhenDocumentBecomesVisible);
+    } else if (event.detail.state === "rejected") {
+        location.reload();
+    }
+}
 
-                if (options.nextRetryIntervalMilliseconds !== undefined) {
-                    let secondsRemaining = Math.round(options.nextRetryIntervalMilliseconds / 1000);
-                    if (secondsToNextAttempt) {
-                        secondsToNextAttempt.innerText = secondsRemaining;
-                    }
+async function retry() {
+    document.removeEventListener("visibilitychange", retryWhenDocumentBecomesVisible);
 
-                    const timer = setInterval(() => {
-                        secondsRemaining--;
-                        if (secondsToNextAttempt) {
-                            secondsToNextAttempt.innerText = secondsRemaining;
-                        }
-                        if (secondsRemaining <= 0) {
-                            clearInterval(timer);
-                        }
-                    }, 1000);
+    try {
+        // Reconnect will asynchronously return:
+        // - true to mean success
+        // - false to mean we reached the server, but it rejected the connection (e.g., unknown circuit ID)
+        // - exception to mean we didn't reach the server (this can be sync or async)
+        const successful = await Blazor.reconnect();
+        if (!successful) {
+            // We have been able to reach the server, but the circuit is no longer available.
+            // We'll reload the page so the user can continue using the app as quickly as possible.
+            const resumeSuccessful = await Blazor.resumeCircuit();
+            if (!resumeSuccessful) {
+                location.reload();
+            } else {
+                reconnectModal.close();
+            }
+        }
+    } catch (err) {
+        // We got an exception, server is currently unavailable
+        document.addEventListener("visibilitychange", retryWhenDocumentBecomesVisible);
+    }
+}
 
-                    modal?.setAttribute('class', 'components-reconnect-repeated-attempt');
-                } else {
-                    modal?.setAttribute('class', 'components-reconnect-show');
-                }
-            },
-            onConnectionUp() {
-                modal?.removeAttribute('class');
-                modal?.close();
-            },
-            onMaximumRetryCountExceeded(_error) {
-                modal?.removeAttribute('class');
-                modal?.setAttribute('class', 'components-reconnect-failed');
-            },
-        },
-    },
-});
+async function resume() {
+    try {
+        const successful = await Blazor.resumeCircuit();
+        if (!successful) {
+            location.reload();
+        }
+    } catch {
+        reconnectModal.classList.replace("components-reconnect-paused", "components-reconnect-resume-failed");
+    }
+}
+
+async function retryWhenDocumentBecomesVisible() {
+    if (document.visibilityState === "visible") {
+        await retry();
+    }
+}
