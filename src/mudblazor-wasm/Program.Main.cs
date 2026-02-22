21810fa2feaedb00d15ee2debcde382f5ea5f159
ScarletKuro <artem.melnikov@live.com>
2026-02-22 16:39:03 +0200

    AddMudServices

--- src/mudblazor-wasm/Program.Main.cs
+++ src/mudblazor-wasm/Program.Main.cs
@@ -11,6 +11,7 @@ public class Program
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
+        builder.Services.AddMudServices();
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         #if(!NoAuth)
 
--- src/mudblazor-wasm/Program.cs
+++ src/mudblazor-wasm/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 #if(!NoAuth)
 
